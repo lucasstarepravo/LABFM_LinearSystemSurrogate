7d10adc75e9258154d774f5bf86063b9c29fab64
Lucas Starepravo <lucas.gerken@hotmail.com>
2023-10-10 16:18:47 +0100

    adding pointing vector and initializing the solution of the weights

--- Functions_LABFM.py
+++ Functions_LABFM.py
@@ -119,8 +119,23 @@ def monomial_power(polynomial):
     return np.array(monomial_exponent)
 
 
-def pointing_v():
-    return
+def pointing_v(m, approximation):
+    """
+
+    :param m:
+    :param approximation:
+    :return:
+    """
+    n = (m**2 + 3*m)/2
+    cd = np.zeros((n,1))
+    if approximation == 'x':
+        cd[0] = 1
+    elif approximation == 'y':
+        cd[1] = 1
+    elif approximation == 'Laplace':
+        cd[2] = 2
+        cd[4] = 4
+    return cd
 
 
 # Scaling matrix still hasn't been applied to the Cd vector
@@ -252,7 +267,8 @@ def calc_abf(nodes, h, m):  # Needs to be written
 def calc_m(basis_func, monomial):
     """
 
-    :param nodes:
+    :param monomial:
+    :param basis_func:
     :return:
     """
 
@@ -266,3 +282,7 @@ def calc_m(basis_func, monomial):
         index = index + 1
 
     return m
+
+
+def solve_weights(m, cd):
+    return
