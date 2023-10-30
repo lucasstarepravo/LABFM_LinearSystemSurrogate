5d1303ccedf871e6417a852469476186932fe668
Lucas Starepravo <lucas.gerken@hotmail.com>
2023-10-30 17:32:17 +0000

    Adding list as input to simulation and initializing convergence plot

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from classes.simulation import Simulation
+from classes.simulation import run
+
+
+def plot_convergence(sim):
+    return
 
 
 if __name__ == '__main__':
-    sim = Simulation(50, 4)
+    total_nodes_list = [100, 100, 100]
+    polynomial_list = [2, 4, 6]
+    results = run(total_nodes_list, polynomial_list)
