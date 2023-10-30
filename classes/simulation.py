90aa69f04d84e83bac246b8ac9178bd72ca092b7
Lucas Starepravo <lucas.gerken@hotmail.com>
2023-10-30 17:29:02 +0000

    Showing lists as input to function

--- classes/simulation.py
+++ classes/simulation.py
@@ -7,17 +7,25 @@ from functions.plot import plot_weights, show_neighbours
 
 class Simulation:
     def __init__(self, total_nodes, polynomial):
-        self.s             = 1.0 / total_nodes
-        self.h             = calc_h(self.s, polynomial)
-        self.coordinates   = create_nodes(total_nodes, self.s, polynomial)
-        self.weights       = Weights(self.coordinates, polynomial, self.h)
+        self.s = 1.0 / total_nodes
+        self.h = calc_h(self.s, polynomial)
+        self.coordinates = create_nodes(total_nodes, self.s, polynomial)
+        self.weights = Weights(self.coordinates, polynomial, self.h)
         self.test_function = TestFunction(self.coordinates, self.weights)
-        self.dtdx_l2       = calc_l2(self.test_function, 'dtdx')
-        self.dtdy_l2       = calc_l2(self.test_function, 'dtdy')
-        self.laplace_l2    = calc_l2(self.test_function, 'Laplace')
+        self.dtdx_l2 = calc_l2(self.test_function, 'dtdx')
+        self.dtdy_l2 = calc_l2(self.test_function, 'dtdy')
+        self.laplace_l2 = calc_l2(self.test_function, 'Laplace')
 
     def plot_neighbours(self, size=8):
         return show_neighbours(self.coordinates, self.weights, size)
 
     def plot_weights(self, size=80, derivative='dtdx'):
         return plot_weights(self.coordinates, self.weights, size, derivative)
+
+
+def run(total_nodes_list, polynomial_list):
+    result = {}
+    for total_nodes, polynomial in zip(total_nodes_list, polynomial_list):
+        sim = Simulation(total_nodes, polynomial)
+        result[(total_nodes, polynomial)] = sim  # .dtdx_l2, sim.dtdy_l2, sim.laplace_l2
+    return result
