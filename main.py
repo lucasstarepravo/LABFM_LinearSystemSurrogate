50ad6e58b39ec8db1921bfd641ea0743edc1cf65
lucasstarepravo <lucas.gerkenstarepravo@postgrad.manchester.ac.uk>
2024-02-14 16:20:24 +0000

    changing values to run simulation

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from classes.simulation import run
 from functions.plot import plot_convergence
 from functions.plot import plot_convergence2
+from shapefunc_surrogate.dd_model import Standardisation
 import dill as pickle
 
 
 if __name__ == '__main__':
-    total_nodes_list = [10, 20, 50, 10, 20, 50]
-    polynomial_list = [2, 2, 2, 4, 4, 4]
+    total_nodes_list = [10, 20, 50, 100, 10, 20, 50, 100]
+    polynomial_list = [2, 2, 2, 2, 4, 4, 4, 4]
     results = run(total_nodes_list, polynomial_list)
 
 
