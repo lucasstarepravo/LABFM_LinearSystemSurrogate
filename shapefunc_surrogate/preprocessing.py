062b3999dd392c6ff7a78d9150850a276c199ad1
lucasstarepravo <lucas.gerkenstarepravo@postgrad.manchester.ac.uk>
2024-03-04 16:23:28 +0000

    Implementing normalisation with std dev withouth mean removal of features

--- shapefunc_surrogate/preprocessing.py
+++ shapefunc_surrogate/preprocessing.py
@@ -2,7 +2,17 @@ import numpy as np
 
 
 def non_dimension(features, h):
-    f_mean = np.mean(features, axis=0, keepdims=True)
+    f_mean = np.mean(features, axis=1, keepdims=True)
     stand_feature = (features - f_mean) / h
 
     return stand_feature, f_mean
+
+
+def stdv_normalisation(features):
+
+    f_mean = np.mean(features, axis=1, keepdims=True)
+    f_std = np.std(features, axis=1, keepdims=True)
+
+    stand_features = (features - f_mean) / f_std
+
+    return stand_features, f_mean, f_std
